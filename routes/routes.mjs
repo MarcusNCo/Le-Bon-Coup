1878006813f9cf38cf90798e0965b335eda2254e
LegrandRemy <legrandremy19@outlook.fr>
2022-10-24 16:59:49 +0200

    first commit

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -5,5 +5,8 @@ const routes = new Router();
 routes.get("/", (req, res) => {
   res.render("index", {message: "Le Bon Coup"});
 })
+  routes.get("/formConnect", (req, res) => {
+    res.render("formConnect", {message: ""});
+})
 
 export default routes;
