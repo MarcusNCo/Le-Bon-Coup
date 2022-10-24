dca370ef303ce9d5de6ac32c6a1355d4ed6a7755
MarcusNCo <levert.marc.a@gmail.com>
2022-10-24 14:53:01 +0200

    dev

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -3,7 +3,7 @@ import {Router} from "express";
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  res.render("index", {message: "Bienvenue sur Le Bon Coup"});
+  res.render("index", {message: "Le Bon Coup"});
 })
 
 export default routes;
