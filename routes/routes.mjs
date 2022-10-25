131a7245a3f579b6ae0008624292b2430593a4b9
Cyril-Crinquette <twousy734@gmail.com>
2022-10-25 12:05:42 +0200

    test branche produit

--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,9 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  res.render("index", {message: "Le Bon Coup"});
-})
+  res.render("index", { message: "Le Bon Coup" });
+});
+
+routes.get("/singleProduct", (req, res) => {
+  res.render("singleProduct");
+});
 
 export default routes;
